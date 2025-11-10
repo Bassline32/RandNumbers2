18a7005f6ddf5eac51d022375908953657959125
Bassline32 <ivandzen911@Gmail.com>
2025-11-10 18:50:50 +0300

    новый клсс юзерНамбер. Фикс проблемы

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-RandomGeneration randomGeneration = new RandomGeneration();
+    RandomGeneration randomGeneration = new RandomGeneration();
+        Scanner scanner = new Scanner(System.in);
+
+
         System.out.println(randomGeneration.getRandom());
     }
 }
+
+
+
+
+
+
+
+
+/*
+    получаем рандомное значение
+    System.out.println(randomGeneration.getRandom());
+ */

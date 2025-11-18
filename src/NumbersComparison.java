36363cb249e09c20ed2eb1ce782333ac765f3d84
Bassline32 <ivandzen911@Gmail.com>
2025-11-18 20:21:26 +0300

    Исправил вывод сообщений пользователю. Сделан счётчик внутри сессии. Также добавил автоматический выход из программы при победе пользователя.

--- src/NumbersComparison.java
+++ src/NumbersComparison.java
@@ -1,20 +1,28 @@
 
 public class NumbersComparison {
     public void compare(UserNumber userNumber, RandomGeneration randomGeneration) throws Exception {
-        try {
-            int userInput = userNumber.getUserNumber();
-            int generatedNumber = randomGeneration.getRandom();
 
-            if (userInput == generatedNumber) {
-                System.out.println("Вы победили");
-            } else {
-                System.out.println("Вы проиграли");
+        int attemptCounter = 0;
+
+        while (true){
+            attemptCounter++;
+            try {
+                int userInput = userNumber.getUserNumber();
+                int generatedNumber = randomGeneration.getRandom();
+
+                if (userInput == generatedNumber) {
+                    System.out.println("Вы победили. Число попыток: " + attemptCounter);
+                    System.out.println(" Сгенерированное число = " + generatedNumber);
+                    break;
+                } else {
+                    System.out.println("Вы проиграли");
+
+                }
+                System.out.print(" Ваше число = " + userInput);
+                System.out.println(" Сгенерированное число = " + generatedNumber);
+            } catch (Exception e) {
+                throw new Exception(e);
             }
-            System.out.print(" Ваше число = " + userInput);
-            System.out.println(" Сгенерированное число = " + generatedNumber);
-        } catch (Exception e) {
-            throw new Exception(e);
         }
     }
 }
-

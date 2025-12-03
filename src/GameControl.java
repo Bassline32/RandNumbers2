7d7abb6d56c9caecaa8531241b4ae82c62d25ebe
Bassline32 <ivandzen911@Gmail.com>
2025-12-03 20:55:16 +0300

    продолжаю развивать логику GameControl

--- src/GameControl.java
+++ src/GameControl.java
@@ -6,9 +6,9 @@ public class GameControl {
     Scanner scanner = new Scanner(System.in);
 
     public void gameControl() {
+
         while (true) {
             try {
-
                 //строки 17-21 отвечают за всю логику игры в виде генераций чисел, сравнения и ввода числа пользователем
                 SelectDifficult selectDifficult = new SelectDifficult();
                 RandomGeneration randomGeneration = selectDifficult.choiceDifficult();
@@ -17,11 +17,22 @@ public class GameControl {
                 numbersComparison.compare(userNumber, randomGeneration);
 
                 //логика продолжения игры и выхода из программы
-                System.out.println("Хотите сыграть ещё раз?");
-                System.out.println(" 1(ДА) 2(НЕТ)");
-                if (scanner.nextInt() != 1) {
-                    System.out.println("Вы вышли из программы");
-                    break;
+                boolean validAnswer = false;
+                while (!validAnswer) {
+
+                    System.out.println("Хотите сыграть ещё раз?");
+                    System.out.println(" (ДА) (НЕТ)");
+                    String userChoice = scanner.nextLine().toLowerCase();
+
+
+                    if (userChoice.equalsIgnoreCase("да")) {
+                        validAnswer = true;
+                    } else if (userChoice.equalsIgnoreCase("нет")) {
+                        System.out.println("Вы вышли из программы");
+                        break;
+                    } else {
+                        System.out.println("Вы ввели некорректное значение");
+                    }
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Вы ввели строку. Введите число");
